ccd9c7197cfdb11a46ff7804881ad44ee036ca9d
g588928812 <128976718+g588928812@users.noreply.github.com>
2023-07-30 07:12:44 +0200

    Create ft.sh

---
+++ ft.sh
@@ -0,0 +1,16 @@
+python finetune.py \
+    --base_model 'decapoda-research/llama-7b-hf' \
+    --data_path 'openassistant_best_replies_train_io.jsonl' \
+    --output_dir './lora-alpaca' \
+    --batch_size 128 \
+    --micro_batch_size 4 \
+    --num_epochs 3 \
+    --learning_rate 1e-4 \
+    --cutoff_len 2000 \
+    --val_set_size 2000 \
+    --lora_r 8 \
+    --lora_alpha 16 \
+    --lora_dropout 0.05 \
+    --lora_target_modules '[q_proj,v_proj]' \
+    --wandb_project "guanaco-lora-a100-colab" \
+    --prompt_template_name "guanaco"
